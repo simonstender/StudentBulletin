93abdf77e9bef700c7d1abfdc7a5d13d3a3928f9
Digy95 <simonstender@outlook.com>
2019-07-02 12:27:48 +0200

    Stuff

--- App.js
+++ App.js
@@ -1,36 +1,38 @@
 import React, {Component} from 'react';
 import { TouchableOpacity, Image, AppRegistry, StyleSheet, Text, View } from 'react-native';
 import {createDrawerNavigator, createStackNavigator, createAppContainer, createSwitchNavigator, createBottomTabNavigator} from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Login from "./view/Login"
 import Bulletin from "./view/Bulletin"
 import Courses from "./view/Courses"
 import Events from "./view/Events"
+import SideMenu from "./navigators/SideMenu"
 
 const BulletinTabNavigator = createBottomTabNavigator({
-  Bulletin,
   Courses,
   Events
 },{
   navigationOptions:({navigation})=>{
     const {routeName} = navigation.state.routes[navigation.state.index]
     return{
-      headerTitle:routeName
+      headerTitle:routeName,
     }
   }
 })
 
-const BulletinStackNavigator = createStackNavigator({
-  BulletinTabNavigator:BulletinTabNavigator
+const AppStackNavigator = createStackNavigator({
+  BulletinTabNavigator:BulletinTabNavigator,
+  LoginScreen: Login,
+  BulletinScreen: Bulletin,
+  CoursesScreen: Courses,
+  EventsScreen: Events,
 },{
-  defaultNavigationOptions: ({navigation}) =>{
+  defaultNavigationOptions: ({navigation}) => {
     return {
       headerLeft: <TouchableOpacity
       onPress={() => navigation.openDrawer()}>
-      <Image
-      style={{ height: 90, width: 90, paddingLeft: 30 }}
-      source={require("./resources/menu-512.png")}
-      />
+      <Icon style={styles.icon} name="user-circle" size={30} color="#7DF0E8" />
       </TouchableOpacity>,
       headerStyle: {
         backgroundColor: '#ADADAD',
@@ -41,68 +43,15 @@ const BulletinStackNavigator = createStackNavigator({
         flex: 1,
         fontSize: 24,
         textAlign: "center",
-        alignSelf: "center"
-       },
-        headerTintColor: '#FFFFFF',
-        drawerLockMode: "locked-closed",
-        gesturesEnabled: false,
+        alignSelf: "center",
+        position: "absolute"
+      },
     }
   }
 })
 
 const AppDrawerNavigator = createDrawerNavigator({
-  BulletinScreen:{screen:BulletinStackNavigator}
-})
-
-AppSwitchNavigator = createSwitchNavigator({
-  LoginScreen:{screen:Login},
-  BulletinScreen:{screen:AppDrawerNavigator}
-})
-
-
-
-const App = createAppContainer(AppSwitchNavigator);
-export default App;
-AppRegistry.registerComponent("App", () => App);
-
-
-/*
-const TopTabNavigator = createMaterialTopTabNavigator({
-  LoginScreen: Login,
-  BulletinScreen: Bulletin,
-},
-{
-  tabBarOptions: {
-  }
-});
-
-const AppStackNavigator = createStackNavigator({
-  TopTabNavigator: TopTabNavigator
-},
-{
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: '#ADADAD',
-      height: 60,
-      borderBottomWidth: 0,
-    },
-    headerTitleStyle: {
-      flex: 1,
-      fontSize: 24,
-      textAlign: "center",
-      alignSelf: "center"
-     },
-      headerTintColor: '#FFFFFF',
-      drawerLockMode: "locked-closed",
-      gesturesEnabled: false,
-    },
-  });
-
-// Menu navigation
-const AppDrawerNavigator = createDrawerNavigator({
-  Dashboard: {
-    screen: AppStackNavigator
-  }
+  Stäng: AppStackNavigator,
 },
 {
   drawerLockMode: "locked-closed",
@@ -111,6 +60,23 @@ const AppDrawerNavigator = createDrawerNavigator({
     labelStyle: {
       fontSize: 24,
       color: '#FFFFFF'},
-    }
-  });
-*/
+    },
+    contentComponent: SideMenu,
+})
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoginScreen: Login,
+  BulletinScreen: AppDrawerNavigator,
+  CoursesScreen: Courses,
+  EventsScreen: Events
+})
+
+const App = createAppContainer(AppSwitchNavigator);
+export default App;
+AppRegistry.registerComponent("App", () => App);
+
+const styles = StyleSheet.create({
+  icon: {
+    paddingLeft: 12,
+  }
+});
